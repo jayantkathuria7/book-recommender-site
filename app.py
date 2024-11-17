fc2e109453aac4cfbc0a21dd8a12bc64d822c7c4
jayantkathuria7 <jayantkathuria7@gmail.com>
2024-11-18 00:06:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request
 import pickle
-import pandas
+import pandas as pd
 import numpy as np
 
-
+# Load your data
 popular_df = pickle.load(open('.pkl files/popular.pkl', 'rb'))
 pt = pickle.load(open('.pkl files/pt.pkl', 'rb'))
 books = pickle.load(open('.pkl files/books.pkl', 'rb'))
 similarity_score = pickle.load(open('.pkl files/similarity_score.pkl', 'rb'))
 
-app = Flask('__name__')
-
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -21,13 +20,11 @@ def index():
                            votes=list(popular_df['num_ratings'].values),
                            rating=list(np.round(popular_df['avg_ratings'].values, 3)))
 
-
 @app.route('/recommend')
 def recommend_ui():
     return render_template('recommend.html')
 
-
-@app.route('/recommend_books',methods=['POST'])
+@app.route('/recommend_books', methods=['POST'])
 def recommend():
     user_input = request.form.get('user_input')
     try:
@@ -41,14 +38,12 @@ def recommend():
             item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
             item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
             item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-
             data.append(item)
+
         return render_template('recommend.html', data=data)
 
-    except (IndexError, ValueError):  # Handle potential errors during data access
-        # Render a different template with an error message
+    except (IndexError, ValueError):
         return render_template('recommend.html', message="Book not found. Please try a different title.")
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
